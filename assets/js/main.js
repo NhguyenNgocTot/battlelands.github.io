831637f948dd3a6b259029a667014ef6d9d9f36b
Ngọc Tốt Nguyễn <tot.nn@hesperiafreepress.com>
2021-12-26 23:44:27 +0700

    update responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,3 +36,52 @@ jQuery(document).ready(function($) {
 
     
 });
+
+jQuery(document).ready(function($) {
+
+    $(document).on('data-char-changed', function() {
+        var index = $('.characters_weapons_right .img_box'),
+            label = $('.characters_weapons_right .content_box .subtitle'),
+            title = $('.characters_weapons_right .content_box .title'),
+            content = $('.characters_weapons_right .content_box .content'),
+            money = $('.characters_weapons_right .content_box .money');
+        setTimeout(function() {
+            index.delay(260).attr('data-aos', 'fade-up');
+        }, 260);
+        setTimeout(function() {
+            index.delay(500).attr('data-aos', 'fade-up').children('img').attr('src', index.data('src'));
+            label.delay(500).text(index.data('label'));
+            title.delay(500).text(index.data('title'));
+            content.delay(500).text(index.data('content'));
+            money.delay(500).text(index.data('money'));
+            index.addClass('aos-init aos-animate');
+            label.addClass('aos-init aos-animate');
+            title.addClass('aos-init aos-animate');
+            content.addClass('aos-init aos-animate');
+            money.addClass('aos-init aos-animate');
+        }, 450);
+    });
+    $('.item_charactor').on('click', function() {
+        if( !$(this).hasClass('selected') ) {
+        var index = $('.characters_weapons_right .img_box'),
+        content_box = $('.characters_weapons_right .content_box');
+        index.attr('data-aos', 'fade-down');
+        index.removeClass('aos-init aos-animate');
+        content_box.children().removeClass('aos-init aos-animate');
+        index.data({
+            'src' : $(this).data('img'),
+            'label' : $(this).data('label'),
+            'title' : $(this).data('title'),
+            'content' : $(this).data('content'),
+            'money' : $(this).data('money'),
+        });
+        if($(this).is('.item_charactor img')) {
+            $(this).parent('.item_charactor').addClass('selected').siblings().removeClass('selected');
+        } else if( $(this).is('.item_charactor') ) {
+            $(this).addClass('selected').siblings().removeClass('selected');
+        }
+        $(document).trigger('data-char-changed');
+
+        }
+    });
+});
